7f7fd1ee8823efab56ff2e1c6ea501a6c6ff1be3
andrewbeav <andrew.bevelhymer@okstate.edu>
2016-11-30 19:29:22 -0600

    Made window show up in right corner

--- TimerWindow.java
+++ TimerWindow.java
@@ -2,14 +2,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class TimerWindow extends JFrame {
+public class TimerWindow extends JFrame implements ActionListener {
 
   JLabel infoLabel;
   JLabel timeText;
 
+  javax.swing.Timer timer;
+  int timerSpeed = 1000, currentTime = 3;
+
   public TimerWindow() {
     super("Timer");
     setSize(150, 200);
+
+    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    GraphicsDevice defaultScreen = ge.getDefaultScreenDevice();
+    Rectangle rect = defaultScreen.getDefaultConfiguration().getBounds();
+
+    setLocation((int)rect.getMaxX(), 0);
 
     Container contentPane = this.getContentPane();
     contentPane.setLayout(new BorderLayout());
@@ -18,9 +27,22 @@ public class TimerWindow extends JFrame {
     infoLabel.setHorizontalAlignment(JLabel.CENTER);
     contentPane.add(infoLabel, BorderLayout.NORTH);
 
-    timeText = new JLabel("3");
+    timeText = new JLabel(Integer.toString(currentTime));
     timeText.setHorizontalAlignment(JLabel.CENTER);
     timeText.setFont(new Font(timeText.getFont().getName(), Font.PLAIN, 100));
     contentPane.add(timeText, BorderLayout.CENTER);
+
+    timer = new javax.swing.Timer(timerSpeed, this);
+    timer.start();
+  }
+
+  public void actionPerformed(ActionEvent event) {
+    Object source = event.getSource();
+
+    if (source == timer) {
+      currentTime--;
+      if (currentTime < 0) currentTime = 3;
+      timeText.setText(Integer.toString(currentTime));
+    }
   }
 }
