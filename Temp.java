b896e83f48edac1a8506d963c4bc9ac6ccc71e1f
andrewbeav <andrewbevelhymer@gmail.com>
2016-12-30 02:39:45 -0600

    Add temporary file for testing the choose image window

---
+++ Temp.java
@@ -0,0 +1,11 @@
+/* temporary file for testing the ChooseImagesWindow */
+
+import javax.swing.*;
+
+public class Temp {
+  public static void main(String[] args) {
+    ChooseImagesWindow gui = new ChooseImagesWindow();
+    gui.setVisible(true);
+    gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+}
